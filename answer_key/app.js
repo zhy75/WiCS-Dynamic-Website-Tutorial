6704b1c843515235e2cb55d944b762b2d1175748
Katelyn Morrison <katelyncmorrison98@gmail.com>
2021-01-25 23:02:03 -0600

    adding answer key and fill in blank code fills

--- answer_key/app.js
+++ answer_key/app.js
@@ -4,14 +4,14 @@ const switcher = document.querySelector('.btn');
 
 // button event listener
 switcher.addEventListener('click', function() {
-    document.body.classList.toggle()
+    document.body.classList.toggle('dark-theme')
 
     var className = document.body.className;
-    if(className == ) {
-        this.textContent = ;
+    if(className == 'light-theme') {
+        this.textContent = "Dark";
     }
     else {
-        this.textContent = ;
+        this.textContent = "Light";
     }
 
 });
@@ -24,9 +24,9 @@ function myFunction() {
   //Finish the function
 
   // Get the value of the input field with id="teamname"
-  
+  x = document.getElementById("teamname").value;
 
-  
+  text = "Your team name idea is " + x;
 
-  
+  document.getElementById("formbtn").innerHTML = text;
 } 
